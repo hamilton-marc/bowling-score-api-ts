109cee5aa21655882fa0178b7620bcdc702bcbe7
Union Hills Software <unionhills@cox.net>
2021-06-26 22:26:07 -0700

    We've got work to do to incorporate rendering the ScoreCardDTO interface.

--- src/cli/score-card.cli.ts
+++ src/cli/score-card.cli.ts
@@ -1,38 +1,107 @@
-import Table from 'cli-table3';
+import CliTable3, { Table } from 'cli-table3';
+import colors from 'colors/safe';
+import { head } from 'lodash';
+import { ThrowTally } from '../models';
 import { ScoreCardDTO } from '../shared';
 
 export class ScoreCardDisplay {
-    private readonly DEFAULT_TABLE_CHARS = { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+    private readonly DEFAULT_TABLE_CHARS = {
+      'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
     , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
     , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
     , 'right': '║' , 'right-mid': '╢' , 'middle': '│' };
-
-    private table!: Table;
 
     public constructor(
         private scoreCard: ScoreCardDTO = { } as ScoreCardDTO
     ) {
     }
 
-    public renderScoreCard(scoreCardInput?: ScoreCardDTO): string {
-        if (scoreCardInput) this.scoreCard = scoreCardInput;
-
-        return this.createTable();
+    public renderScoreCard(maxThrows: number = ThrowTally.MAX_THROWS): string {
+        return this.createTable(maxThrows);
     }
 
-    private createTable(): string {
+    private createHeaderRow(): Array<any> {
+        const row = [];
+
+        row.push(colors.green('Frame'));
+
+        for (let i: number = 0; i < ThrowTally.MAX_FRAMES; i++) {
+            const colSpan: number = i < ThrowTally.MAX_FRAMES - 1 ? 2 : 3;
+            const content: string = (i + 1).toString();
+
+            row.push({
+                hAlign: 'center'
+              , colSpan: colSpan
+              , content: colors.green(content)
+            });
+        }
+
+        return row;
+    }
+
+    private renderThrow() {
+
+    }
+
+    private createThrowsRow(maxThrows: number): Array<any> {
+        const row = [];
+
+        row.push(colors.green('Throws'));
+
+        for (let i: number = 0; i < ThrowTally.MAX_THROWS; i++) {
+            let content: string = ' ';
+/*
+            if (i < maxThrows) {
+                // A little messy... we need to figure out which throw index to use
+                // in the collection of frames
+                const frameThrowIndex = i % ( i < ThrowTally.MAX_THROWS - 1 ? 2 : 3);
+
+                this.scoreCard.frameScores[maxThrows / 2].throwValues[frameThrowIndex];
+            }
+*/
+            row.push({
+                hAlign: 'center'
+              , content: content
+            });
+        }
+
+        return row;
+    }
+
+    private createFrameScoreRow(maxFrames: number): Array<any> {
+        const row = [];
+
+        row.push(colors.green('Score'));
+
+        for (let i: number = 0; i < ThrowTally.MAX_FRAMES; i++) {
+            const colSpan: number = i < ThrowTally.MAX_FRAMES - 1 ? 2 : 3;
+            const content: string = ' ';
+/*
+            if (i < maxFrames) {
+                this.scoreCard.frameScores[i].score.toString();
+            }
+*/
+            row.push({
+                hAlign: 'center'
+              , colSpan: colSpan
+              , content: content
+            });
+        }
+
+        return row;
+    }
+
+    private createTable(maxThrows: number): string {
         let renderedTable: string = '';
-        let table = new Table({
+        let table: Table = new CliTable3({
             chars: this.DEFAULT_TABLE_CHARS
 //        , colWidths: [30, 30]
         });
 
-        table.push(            
-            ['Frame',  {hAlign: 'center', colSpan:2, content:'1'}, {hAlign: 'center', colSpan:2, content:'2'},
-                       {hAlign: 'center', colSpan:2, content:'3'}, {hAlign: 'center', colSpan:3, content:'4'}]
-          , ['Throws', '1', '2', '3', '4', '5', '6', '7', '8', '-']
-          , ['Score',  {hAlign: 'center', colSpan:2, content:'3'}, {hAlign: 'center', colSpan:2, content:'10'},
-                       {hAlign: 'center', colSpan:2, content:'21'}, {hAlign: 'center', colSpan:3, content:'36'}]
+        table.push(
+            this.createHeaderRow(),
+            this.createThrowsRow(maxThrows),
+            this.createFrameScoreRow(maxThrows / 2)
         );
 
         renderedTable += table.toString() +'\n';
