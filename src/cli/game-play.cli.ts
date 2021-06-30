f96fb78f935501422f6f70bba51d093b056a28cc
Union Hills Software <unionhills@cox.net>
2021-06-30 09:57:57 -0700

    Still working on incorporating all of the game rules

--- src/cli/game-play.cli.ts
+++ src/cli/game-play.cli.ts
@@ -3,11 +3,13 @@ import { ScoreCard, ThrowTally } from '../models';
 import { BowlingScoreCalculator } from '../services';
 
 export class GamePlay {
-    private scoreCalculator = new BowlingScoreCalculator();
+    ;
     private throwTally = new ThrowTally();
     private scoreCard = new ScoreCard();
 
-    constructor() { }
+    constructor(
+        private scoreCalculator = new BowlingScoreCalculator()
+    ) { }
 
     private reset(): void {
         this.throwTally = new ThrowTally();
@@ -17,13 +19,32 @@ export class GamePlay {
     public async newGame(): Promise<ScoreCard> {
         this.reset();
 
-        for (let i=0; i < ThrowTally.MAX_THROWS - 1; i++) {
-            const frameIndex: number = Math.floor(i / 2);
-            const frameThrowIndex: number = i % 2;
+        for (let i=0; i < ThrowTally.MAX_THROWS; i++) {
+            const frameIndex: number = Math.floor(i / 2) - (i === ThrowTally.MAX_THROWS - 1 ? 1 : 0);
+            const frameThrowIndex: number = i % (frameIndex < ThrowTally.MAX_FRAMES - 1 ? 2 : 3);
             let maxPins = ThrowTally.MAX_PINS;
 
-            if (frameThrowIndex === 1) {
+            if (frameThrowIndex > 0) {
                 maxPins = ThrowTally.MAX_PINS - this.throwTally.getThrow(i-1);
+            }
+
+            if (frameIndex === ThrowTally.MAX_FRAMES - 1) {
+
+                console.log(`frameThrowIndex = ${frameThrowIndex}`);
+                console.log(`this.throwTally.getThrow(i-1) + this.throwTally.getThrow(i-2) = ${this.throwTally.getThrow(i-1) + this.throwTally.getThrow(i-2)}`);
+
+                // if we're in the last frame and on the 3rd throw...
+                // if the user hasn't had at least a spare,
+                // then it's game over.
+                if (frameThrowIndex > 1 &&
+                    this.throwTally.getThrow(i-1) + this.throwTally.getThrow(i-2) < ThrowTally.MAX_PINS) {
+                    this.throwTally.setThrow(i, 0);
+                    continue;
+                }
+
+                if (frameThrowIndex > 0 && this.throwTally.getThrow(i-1) === ThrowTally.MAX_THROWS) {
+                    maxPins = ThrowTally.MAX_PINS;
+                }
             }
 
             const questions: PromptObject<string>[] = [{
